335fa5452738861508e9b113c3f1cd425c5ca1d4
trace2798 <113078518+trace2798@users.noreply.github.com>
2025-07-25 16:12:45 +0530

    basic working

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,13 +1,21 @@
-import { togetherai } from "@ai-sdk/togetherai";
-import { streamText, UIMessage, convertToModelMessages } from "ai";
+// import { togetherai } from "@ai-sdk/togetherai";
+import { convertToModelMessages, streamText, UIMessage } from "ai";
+import { createTogetherAI } from '@ai-sdk/togetherai';
 
 export const maxDuration = 30;
 
+
+const togetherai = createTogetherAI({
+  apiKey: process.env.TOGETHER_AI_API!,
+});
+
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
+
   const result = streamText({
     model: togetherai("meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8"),
     messages: convertToModelMessages(messages),
   });
+
   return result.toUIMessageStreamResponse();
 }
